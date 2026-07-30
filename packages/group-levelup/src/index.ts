a3d987f5f1c90ba2791f1312e3290d94cff91e4c
oppenheymu <oppenheymu@gmail.com>
2026-07-30 14:44:17 +0800

    feat(group-levelup): 继续初始化插件

---
+++ packages/group-levelup/src/index.ts
@@ -0,0 +1,17 @@
+import { type Context, Schema } from 'koishi';
+
+export const name = 'group-levelup';
+
+export const usage = `
+<div style="border-radius: 10px; border: 1px solid #ddd; padding: 16px; margin-bottom: 20px; box-shadow: 0 2px 5px rgba(0,0,0,0.1);">
+  <h2 style="margin-top: 0; color: #4a6ee0;">📖 使用说明</h2>
+  <p>🗑️ 本插件通过群打卡和冒泡、续火来快速升级群等级</p>
+  <p>⚠️ 可能会被群管理 <strong>制裁</strong> ，谨慎使用！</p>
+</div>
+
+<div style="border-radius: 10px; border: 1px solid #ddd; padding: 16px; margin-bottom: 20px; box-shadow: 0 2px 5px rgba(0,0,0,0.1);">
+  <h2 style="margin-top: 0; color: #4a6ee0;">💬 交流与反馈</h2>
+  <p>🌟 喜欢这个插件？欢迎加入 QQ 群 <a href="https://qm.qq.com/q/WngX4RQoca" style="color:#4a6ee0;text-decoration:none;"><strong>1071284605</strong></a>【晓基地插件工坊】进行交流</p>
+  <p>🐛 遇到问题？欢迎在群内反馈，或点击 <a href="https://qm.qq.com/q/WngX4RQoca" style="color:#4a6ee0;text-decoration:none;">此链接</a> 加入群聊</p>
+</div>
+`;
